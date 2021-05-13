e3a9df6e605d92f3e279f799f694d4fc6bb014da
André Dias <andremssdias@hotmail.com>
2021-05-13 17:23:45 +0200

    Added timer

--- pong.py
+++ pong.py
@@ -1,19 +1,26 @@
 import pygame
+import random
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y, player_1_score, player_2_score
+    global ball_speed_x, ball_speed_y
+    global player_1_score, player_2_score
+    global score_time
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.y >= screen_height or ball.y <= 0:
         ball_speed_y *= -1
+
     if ball.right >= screen_width:
         player_2_score += 1
-        ball_restart()
+
+        # How long the game as been running
+        score_time = pygame.time.get_ticks() 
+
     if ball.left <= 0:
         player_1_score += 1
-        ball_restart()
+        score_time = pygame.time.get_ticks()
 
     if ball.colliderect(player_1) or ball.colliderect(player_2):
         ball_speed_x *= -1
@@ -38,8 +45,30 @@ def player_2_animation():
         player_2.bottom = screen_height
 
 
-def ball_restart():
+def ball_start():
+    global ball_speed_x, ball_speed_y, score_time
+
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width/2, screen_height/2)
+
+    if current_time - score_time < 1000:
+        number_three = game_font.render("3", False, light_grey)
+        screen.blit(number_three, (screen_width/2 - 10, screen_height/2 + 20))
+    if 1000 < current_time - score_time < 2000:
+        number_two = game_font.render("2", False, light_grey)
+        screen.blit(number_two, (screen_width/2 - 10, screen_height/2 + 20))
+    if 2000 < current_time - score_time < 3000:
+        number_one = game_font.render("1", False, light_grey)
+        screen.blit(number_one, (screen_width/2 - 10, screen_height/2 + 20))
+    
+    if current_time - score_time < 2100:
+        ball_speed_x, ball_speed_y = 0, 0
+    else:
+        ball_speed_x = 4 * random.choice((1, -1))
+        ball_speed_y = 4 * random.choice((1, -1))
+        score_time = None
+
+
 
 
 # Initialize pygame
@@ -69,6 +98,8 @@ player_1_score = 0
 player_2_score = 0
 game_font = pygame.font.Font("freesansbold.ttf", 32)
 
+# Score timer
+score_time = True
 
 running = True
 while running:
@@ -97,6 +128,9 @@ while running:
     pygame.draw.rect(screen, light_grey, player_2)
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
+
+    if score_time:
+        ball_start()
 
     player_1_text = game_font.render(f"{player_1_score}", False, light_grey)
     screen.blit(player_1_text, (screen_width/2 + 15, screen_height/2 - 16))
